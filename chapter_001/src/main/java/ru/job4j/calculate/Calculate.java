2972fda4cf3685f2b2aace709886bc44c58916e9
Alexander Gnatovsky <skysurfersky@gmail.com>
2019-04-15 23:56:40 +0300

    1.3. JavaDoc. Code convention[#133511]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,18 @@
 package ru.job4j.calculate;
-
+	 /**
+	 * Calculate.
+	 *
+	 * @author Lex Gnatovsky
+	 */
+	 
 public class Calculate {
-
+	
+	 /**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-
+	
 }
