43e962a76991f4549a0fbf51f574fc5288028d59
Alexander Gnatovsky <skysurfersky@gmail.com>
2019-04-16 23:54:42 +0300

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Lex Gnatovsky
+ * @version 0.2
+ * @since 4.16.19
+ */
+
+public class CalculateTest {
+ /**
+  * Test echo
+  */
+ @Test
+ public void whenTakeNameThenThreeEchoPlusName () {
+ 	String input = "Lex Gnatovsky";
+ 	String expect = "Echo, echo, echo : Lex Gnatovsky";
+ 	Calculate calc = new Calculate();
+ 	String result = calc.echo(input);
+ 	assertThat(result, is(expect));
+ }
+}
