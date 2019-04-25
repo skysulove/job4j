7fff4e0441565e49403872d70d0b13635bcbce0b
Alexander Gnatovsky <skysurfersky@gmail.com>
2019-04-25 07:02:23 +0300

    3.1. Элементарный калькулятор(fullv).[#133501]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,17 +1,44 @@
 package ru.job4j.calculator;
+/**
+ * Calculator.
+ *
+ * @author Lex Gnatovsky
+ */
 
 public class Calculator {
+    /**
+     * Method  add.
+     @param first First value.
+     @param second Second value.
+     @return first + second.
+     */
     public double add(double first, double second) {
         return first + second;
     }
-
+    /**
+     * Method  subtract.
+     @param first First value.
+     @param second Second value.
+     @return first - second.
+     */
     public double subtract(double first, double second) {
         return first - second;
     }
-
+    /**
+     * Method  div.
+     @param first First value.
+     @param second Second value.
+     @return first / second.
+     */
     public double div(double first, double second) {
         return first / second;
     }
+    /**
+     * Method  multiple.
+     @param first First value.
+     @param second Second value.
+     @return first * second.
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
---
+++ chapter_001/src/test/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculator task.
+ *
+ * @author Lex Gnatovsky
+ * @version 0.2
+ * @since 4.15.19
+ */
+package ru.job4j.calculator;
