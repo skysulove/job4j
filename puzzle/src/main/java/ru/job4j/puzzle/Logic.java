e5f2838f14c305ed9ea5d0f5502c2889ab9054d4
Alexander Gnatovsky <skysurfersky@gmail.com>
2019-06-25 00:27:10 +0300

    Пазлы. Тестовое задание.[#133517]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -37,12 +37,12 @@ public class Logic {
         return rst;
     }
 
-    public boolean isFree(Cell ... cells) {
+    public boolean isFree(Cell... cells) {
         boolean result = cells.length > 0;
         for (Cell cell : cells) {
             if (this.findBy(cell) != -1) {
-               result = false;
-               break;
+                result = false;
+                break;
             }
         }
         return result;
@@ -69,9 +69,24 @@ public class Logic {
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
+        for (int row = 0; row < table.length; row++) {
+            int count1 = 0;
+            int count2 = 0;
+            for (int cell = 0; cell < table.length; cell++) {
+                if (table[row][cell] == 1) {
+                    count1++;
+                }
+                if (table[cell][row] == 1) {
+                    count2++;
+                }
+                if ((count1 == table.length) || (count2 == table.length)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
         return result;
     }
-
     public int[][] convert() {
         int[][] table = new int[this.size][this.size];
         for (int row = 0; row != table.length; row++) {
