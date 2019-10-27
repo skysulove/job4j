92492a6548e45fb18254d147fdb15fefed34de98
Lex Gnatovsky <a.gnatovsky@mail.ru>
2019-10-28 00:06:26 +0300

    1.5. Поля объекта [#166083]

---
+++ chapter_002/src/main/java/oop/Cat.java
@@ -0,0 +1,29 @@
+package oop;
+
+public class Cat {
+    private String food;
+    private String name;
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+    public void eat(String meat) {
+        this.food = meat;
+    }
+    public void  nick() {
+        System.out.println(this.name);
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
